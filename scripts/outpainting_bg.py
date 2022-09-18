d3b38a4205036bcd1ebed9ec26723e088be96e5d
chand1012 <chandler@chand1012.dev>
2022-09-18 13:00:17 -0400

    Organize script

--- scripts/outpainting_bg.py
+++ scripts/outpainting_bg.py
@@ -1,12 +1,14 @@
+import random
 import sys
 import time
 import os
+from typing import Tuple
 import shutil
-import argparse
 from ldm.outpainting_utils import edge_pad
 from ldm.generate import Generate
 from ldm.dream.devices import choose_torch_device
 from PIL import Image
+from PIL.Image import Image as ImageClass
 import numpy as np
 
 output_dir = os.path.join(os.getcwd(), 'outputs', 'outpainting')
@@ -14,6 +16,7 @@ working_dir = os.path.join(output_dir, 'tmp')
 device = 'mps' if sys.platform == 'darwin' else choose_torch_device()
 prompt = 'astronaut floating in space. planets in the background. 35 mm. nightmare. depth of field. vivid colors. photorealistic. photography. 8k. artstation. ultra detailed. octane. bokeh.'
 height = 512
+img = ''
 width = 512
 steps = 50
 upscale = True
@@ -23,7 +26,7 @@ vertical = True
 jpeg = True
 
 
-def edgify(img: np.ndarray) -> Image:
+def edgify(img: np.ndarray) -> ImageClass:
     '''returns an image filled from edge provided'''
     mask = img[:, :, 3]
     rgb_img = img[:, :, 0:3]
@@ -43,23 +46,14 @@ def outpaint(generator: Generate, image_path: str, mask_path: str, prompt: str, 
     return output[0][0]
 
 
-if __name__ == '__main__':
-    if not os.path.exists(working_dir):
-        os.makedirs(working_dir)
-
-    sd = Generate(device_type=device)
-    # to make a 16:9 image at 512 generation, we only need to expand 200 pixels in either direction
-    # first, generate a 512x512 image
-
-    initial_image = sd.txt2img(
-        prompt=prompt, height=height, width=width, steps=steps)
-
-    image_path, seed = initial_image[0]
-    # open the image using PIL
-    print('Loading image...')
+def process_image(image_path: str, vertical: bool = False) -> Tuple[str, str, str, str]:
     image = Image.open(image_path).convert('RGBA')
 
-    print('Processing image...')
+    # if the input image is not 512x512, resize it
+    if image.size != (512, 512):
+        print('Resizing image to 512x512')
+        image = image.resize((512, 512), 1)
+
     right_side = Image.new('RGBA', (512, 512), (0, 0, 0, 0))
     left_side = Image.new('RGBA', (512, 512), (0, 0, 0, 0))
 
@@ -89,25 +83,15 @@ if __name__ == '__main__':
     right_side_edged.save(right_side_edged_path)
     left_side_edged.save(left_side_edged_path)
 
-    print('Outpainting...')
+    return right_side_mask_path, left_side_mask_path, right_side_edged_path, left_side_edged_path
 
-    right_side_outpainted_path = os.path.join(
-        working_dir, 'right_side_outpainted.png')
-    left_side_outpainted_path = os.path.join(
-        working_dir, 'left_side_outpainted.png')
 
-    # now, we need to generate the images
-    right_side_outpainted_path = outpaint(sd, right_side_edged_path,
-                                          right_side_mask_path, prompt, name=right_side_outpainted_path)
-    left_side_outpainted_path = outpaint(sd, left_side_edged_path,
-                                         left_side_mask_path, prompt, name=left_side_outpainted_path)
+def combine_images(right_path: str, left_path: str, vertical=False, jpeg=False) -> Tuple[ImageClass, str, str]:
 
-    print("Combining images...")
-    # now, we need to combine the images
     right_side_outpainted = Image.open(
-        right_side_outpainted_path).convert('RGBA')
+        right_path).convert('RGBA')
     left_side_outpainted = Image.open(
-        left_side_outpainted_path).convert('RGBA')
+        left_path).convert('RGBA')
 
     image_mode = 'RGB' if jpeg else 'RGBA'
 
@@ -133,22 +117,65 @@ if __name__ == '__main__':
 
     final_image.save(final_image_path)
 
+    return final_image, final_image_path, final_image_upscaled_path
+
+
+def run_upscale(sd: Generate, input_image: ImageClass, input_seed: int, input_path: str, output_path: str) -> None:
+    # callback for upscaling
+    def cb(image, seed, upscaled=None):
+        if not upscaled:
+            print("Upscaling failed. Saving original image...")
+            return
+        # convert the image to jpeg
+        image.save(output_path)
+
+        if not keep_original:
+            os.remove(input_path)
+
+    sd.upscale_and_reconstruct(
+        image_list=[(input_image, input_seed)], image_callback=cb, upscale=(4, 0.75))
+
+
+if __name__ == '__main__':
+
+    if not os.path.exists(working_dir):
+        os.makedirs(working_dir)
+
+    sd = Generate(device_type=device)
+    # to make a 16:9 image at 512 generation, we only need to expand 200 pixels in either direction
+
+    image_path = img
+    seed = random.randint(0, 4_000_000)
+    if not img:
+        initial_image = sd.txt2img(
+            prompt=prompt, height=height, width=width, steps=steps)
+        image_path, seed = initial_image[0]
+    # open the image using PIL
+    print('Processing image...')
+    right_side_mask_path, left_side_mask_path, right_side_edged_path, left_side_edged_path = process_image(
+        image_path, vertical=vertical)
+
+    print('Outpainting...')
+
+    right_side_outpainted_path = os.path.join(
+        working_dir, 'right_side_outpainted.png')
+    left_side_outpainted_path = os.path.join(
+        working_dir, 'left_side_outpainted.png')
+
+    # now, we need to generate the images
+    right_side_outpainted_path = outpaint(sd, right_side_edged_path,
+                                          right_side_mask_path, prompt, name=right_side_outpainted_path)
+    left_side_outpainted_path = outpaint(sd, left_side_edged_path,
+                                         left_side_mask_path, prompt, name=left_side_outpainted_path)
+
+    print("Combining images...")
+    final_image, final_image_path, final_image_upscaled_path = combine_images(
+        right_side_outpainted_path, left_side_outpainted_path, vertical=vertical, jpeg=jpeg)
+
     if upscale:
         print("Upscaling...")
-
-        # callback for upscaling
-        def cb(image, seed, upscaled=None):
-            if not upscaled:
-                print("Upscaling failed. Saving original image...")
-                return
-            # convert the image to jpeg
-            image.save(final_image_upscaled_path)
-
-            if not keep_original:
-                os.remove(final_image_path)
-
-        sd.upscale_and_reconstruct(
-            image_list=[(final_image, 10000000)], image_callback=cb, upscale=(4, 0.75))
+        run_upscale(sd, final_image, seed, final_image_path,
+                    final_image_upscaled_path)
 
     if clean:
         print("Cleaning up....")
